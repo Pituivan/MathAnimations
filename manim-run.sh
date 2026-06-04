a7d488ec5007352125efccae414cf79d36ddc1b6
Pituivan <ivangranada00@gmail.com>
2026-06-04 21:52:15 +0200

    Add `background_color` optional flag to `manim-run.sh`

--- manim-run.sh
+++ manim-run.sh
@@ -5,12 +5,14 @@ SCENE="$(basename "$FILE" .py)"
 
 QUALITY="0"
 FPS="60"
+BACKGROUND_COLOR=""
 
 shift
 while [[ $# -gt 0 ]]; do
     case "$1" in
         --quality) QUALITY="$2"; shift 2 ;;
         --fps) FPS="$2"; shift 2 ;;
+        --background_color) BACKGROUND_COLOR="$2"; shift 2 ;;
     esac
 done
 
@@ -24,4 +26,14 @@ case "$QUALITY" in
       ;;
 esac
 
+
+if [[ -n "$BACKGROUND_COLOR" ]]; then
+	cat > manim.cfg <<-EOF
+	[CLI]
+	background_color = $BACKGROUND_COLOR
+	EOF
+fi
+
 python -m manim "$Q" -p "$FILE" "$SCENE" --fps "$FPS"
+
+rm -f manim.cfg
