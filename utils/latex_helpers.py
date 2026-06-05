6a254d32f43591d90319d749c924c82f5d7aaa07
Pituivan <ivangranada00@gmail.com>
2026-06-05 21:53:09 +0200

    Add option to `DerivationStep` to use any kind of matching transform as transition

--- utils/latex_helpers.py
+++ utils/latex_helpers.py
@@ -1,4 +1,5 @@
 from manim import Scene, MathTex, TransformMatchingTex
+from manim.animation.transform_matching_parts import TransformMatchingAbstractBase
 
 
 START_G = r"\left."
@@ -6,7 +7,8 @@ END_G = r"\right."
 
 
 class DerivationStep:
-    def __init__(self, *tex_strings: str, transition_duration: float = 1, delay: float = 0):
+    def __init__(self, *tex_strings: str, transition_duration: float = 1, delay: float = 0,
+                 transform_type: type[TransformMatchingAbstractBase] = TransformMatchingTex):
         """
         Parameters:
             *tex_strings (str):
@@ -22,7 +24,7 @@ class DerivationStep:
         self.tex = MathTex(*tex_strings)
         self.transition_duration = transition_duration
         self.delay = delay
-
+        self.transform_type = transform_type
 
 def derive_equation(scene: Scene, base_equation: MathTex, steps: list[DerivationStep]) -> MathTex:
     """
@@ -34,7 +36,7 @@ def derive_equation(scene: Scene, base_equation: MathTex, steps: list[Derivation
     current_tex = base_equation
     for next_step in steps:
         if next_step.delay: scene.wait(next_step.delay)
-        scene.play(TransformMatchingTex(
+        scene.play(next_step.transform_type(
             current_tex, next_step.tex,
             run_time=next_step.transition_duration
         ))
